1149eee1dfe3bf90a452d3bedd33362d9e1cd73b
okita <t-okita@netoffice.obisan.co.jp>
2025-05-07 14:02:28 +0900

    init

---
+++ Split/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using Split.Views;
+
+namespace Split.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+        private string _title = "SMAI - Split";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(Dashboard));
+
+        }
+    }
+}
---
+++ Split/Views/Dashboard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Split.Views
+{
+    /// <summary>
+    /// Interaction logic for Dashboard
+    /// </summary>
+    public partial class Dashboard : UserControl
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
